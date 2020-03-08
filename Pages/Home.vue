3fb954cfb30a55a072b23f9f2173e8890d7d1334
Nitish Kumar <nitishkumar@noeticitservices.com>
2020-03-08 10:09:55 +0530

    Added home page

--- Pages/Home.vue
+++ Pages/Home.vue
@@ -1,70 +1,33 @@
 <template>
-    <div class="flex-center position-ref full-height">
-        <div class="top-right links">
-            <router-link to="/">Home</router-link>
-            <router-link to="/">Login</router-link>
-            <router-link to="/register" >Register</router-link>
+    <div class="block">
+        <div class="absolute mt-4 right-0 px-10">
+            <router-link to="/" class="font-serif subpixel-antialiased font-normal tracking-normal leading-loose p-2 text-gray-600">Home</router-link>
+            <router-link to="/login" class="font-serif subpixel-antialiased font-normal tracking-normal leading-loose p-2 text-gray-600">Login</router-link>
+            <router-link to="/register" class="font-serif subpixel-antialiased font-normal tracking-normal leading-loose p-2 text-gray-600">Register</router-link>
         </div>
 
-        <div class="content">
-            <div class="title m-b-md">
-                Nits Editor
+        <div class="block flex h-screen">
+            <div class="m-auto">
+                <div class="font-serif subpixel-antialiased font-medium text-6xl text-gray-800 tracking-wider leading-4 items-center text-center">
+                    Nits Editor
+                </div>
+
+                <div class="text-gray-400 mt-6 items-center text-center">
+                    <a href="https://nitseditor.com/docs" class="font-serif subpixel-antialiased font-normal tracking-normal leading-loose text-gray-600 p-4">Docs</a>
+                    <a href="https://github.com/noeticit/nitseditor" class="font-serif subpixel-antialiased font-normal tracking-normal leading-loose text-gray-600 p-4">GitHub</a>
+                </div>
             </div>
 
-            <div class="links">
-                <a href="https://nitseditor.com/docs">Docs</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Home"
     }
 </script>
 
 <style scoped>
-    .full-height {
-        height: 100vh;
-    }
 
-    .flex-center {
-        align-items: center;
-        display: flex;
-        justify-content: center;
-    }
-
-    .position-ref {
-        position: relative;
-    }
-
-    .top-right {
-        position: absolute;
-        right: 10px;
-        top: 18px;
-    }
-
-    .content {
-        text-align: center;
-    }
-
-    .title {
-        font-size: 84px;
-    }
-
-    .links > a {
-        color: #636b6f;
-        padding: 0 25px;
-        font-size: 13px;
-        font-weight: 600;
-        letter-spacing: .1rem;
-        text-decoration: none;
-        text-transform: uppercase;
-    }
-
-    .m-b-md {
-        margin-bottom: 30px;
-    }
 </style>
