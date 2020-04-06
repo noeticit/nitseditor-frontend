edd102b944f76228c364a6c0b3d51f973dd26f66
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-04-06 10:55:45 +0530

    some changes in button

---
+++ Components/Base/Alerts/BaseBannerAlert.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-1/3 absolute bg-blue-100 right-0 rounded border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+        <p class="font-bold">Informational message</p>
+        <p class="text-sm">Some additional text to explain said message.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseBannerAlert"
+    }
+</script>
+
+<style scoped>
+
+</style>
