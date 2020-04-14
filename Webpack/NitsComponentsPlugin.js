442f046775b0f4208ba5f9fdcba1c67bbc6fd780
Nitish Kumar <nitishkumar@noeticitservices.com>
2020-04-14 13:30:12 +0530

    Plugins author name and plugin name prefix to component import

--- Webpack/NitsComponentsPlugin.js
+++ Webpack/NitsComponentsPlugin.js
@@ -16,24 +16,26 @@ function exportNitsEditorComponents() {
     generateComponents(path.resolve(__dirname, './../Components'), 'NitsComponents');
 
     //Plugins components...
-    generateComponents(path.resolve('./plugins'), 'Plugins');
+    generatePluginsComponents();
 
-    fs.writeFileSync(path.resolve(__dirname, './Components/components.js'), filecontents);
+    fs.writeFileSync(path.resolve(__dirname, './../Components/components.js'), filecontents);
     
 }
 
-function generateComponents(directory, prefix) {
+function generateComponents(directory, prefix, componentPrefix = '') {
 
     fs.readdirSync(directory, {withFileTypes: true}).forEach((entry) => {
         let fullPath = path.join(directory, entry.name);
         if(entry.isDirectory()) {
-            generateComponents(fullPath, prefix+'/'+entry.name);
+            generateComponents(fullPath, prefix+'/'+entry.name, componentPrefix);
         }
         else if(entry.isFile()){
             if(entry.name.includes('.vue')) {
-                let componentName = entry.name.replace('.vue', '');
+                let comp = entry.name.replace('.vue', '');
 
-                filecontents = filecontents + `Vue.component(\'${componentName}\', () => import(\'${prefix}/${entry.name}\'));\n`;
+                let componentName = comp.charAt(0).toUpperCase() + comp.slice(1);
+
+                filecontents = filecontents + `Vue.component(\'${componentPrefix + componentName}\', () => import(\'${prefix}/${entry.name}\'));\n`;
             }
         } else {
             // apparently there are some other things that could be here
@@ -43,6 +45,27 @@ function generateComponents(directory, prefix) {
     });
 
     return filecontents;
+}
+
+function generatePluginsComponents() {
+
+    fs.readdirSync(path.resolve('./plugins'), {withFileTypes: true}).forEach((entry) => {
+        let pluginAuthorPath = path.join('./plugins', entry.name);
+        let authorName = entry.name;
+        if(entry.isDirectory()) {
+            fs.readdirSync(pluginAuthorPath, {withFileTypes: true}).forEach((entry) => {
+                let pluginpath = path.join(pluginAuthorPath, entry.name+'/components');
+                let pluginName = entry.name;
+                if(entry.isDirectory()) {
+
+                    let authorPrefix = authorName.charAt(0).toUpperCase() + authorName.toLowerCase().slice(1);
+                    let pluginPrefix = pluginName.charAt(0).toUpperCase() + pluginName.toLowerCase().slice(1);
+
+                    generateComponents(pluginpath, 'Plugins/'+authorName+'/'+pluginName+'/components', authorPrefix+pluginPrefix)
+                }
+            })
+        }
+    })
 }
 
 module.exports = NitsComponentsPlugin;
