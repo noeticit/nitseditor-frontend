61748ad201b4936904810e5e630b33dfcca8d9a2
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-04-20 19:09:35 +0530

    changes

--- Pages/permissions.vue
+++ Pages/permissions.vue
@@ -19,7 +19,7 @@
             </div>
             <div>
                 <div >
-                    <input class="ml-3 mt-2 mb-2 text-xs py-1 px-4 border rounded w-2/4" placeholder="Add User....." type="text">
+                    <input class="ml-3 mt-2 mb-2 text-xs py-1 px-4 border rounded w-2/4" placeholder="Add Users . . . ." type="text">
                 </div>
                 <table class="min-w-full">
                     <tr class="bg-teal-500 h-12">
@@ -55,14 +55,14 @@
                             </div>
                         </th>
                     </tr>
-                    <tr v-for="index in 3" :key="index">
-                        <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal">Permission</td>
+                    <tr v-for="(item,index) in tableData">
+                        <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal">{{item.name}}</td>
                         <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal"><input type="checkbox"></td>
                         <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal"><input type="checkbox"></td>
                         <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal"><input type="checkbox"></td>
                         <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal"><input type="checkbox"></td>
                         <td class="h-8 text-center border-b border-gray-200 text-sm text-gray-700 antialiased leading-tight tracking-normal  font-sans font-normal">
-                            <a @click.prevent="removeElement()" class=" " href="#">
+                            <a @click.prevent="removeElement()" href="#">
                                 <svg class="inline-block text-teal-600 fill-current w-4 h-4 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M10 1.6a8.4 8.4 0 100 16.8 8.4 8.4 0 000-16.8zm4.79 11.46l-1.73 1.73L10 11.73l-3.06 3.06-1.73-1.73L8.27 10 5.21 6.94 6.94 5.2 10 8.27l3.06-3.06 1.73 1.73L11.73 10l3.06 3.06z"/>
                                 </svg>
@@ -81,6 +81,7 @@
         data() {
             return {
                 breadcrumbs: ['Permissions'],
+                tableData:[{name:''}]
             }
         },
         components: {
@@ -92,12 +93,13 @@
             }
         },
         created() {
-            console.log(this.$router.options.routes.map((d) => {
+           this.tableData=this.$router.options.routes.map((d) => {
                 return {
                     name: d.name,
-                    route: d.path
+                    // route: d.path
                 }
-            }))
+            })
+            console.log(this.tableData);
         }
     }
 </script>
