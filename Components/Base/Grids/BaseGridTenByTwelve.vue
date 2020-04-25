46f11bcc336cf49b37c5ea0ed028457561983968
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-04-25 15:33:32 +0530

    somechanges

---
+++ Components/Base/Grids/BaseGridTenByTwelve.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-10/12 h-full bg-white rounded-lg  rounded shadow-lg">
+        <div class="text-center p-24 font-serif">
+            TEN BY TWELVE
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseGridTenByTwelve"
+    }
+</script>
+
+<style scoped>
+
+</style>
