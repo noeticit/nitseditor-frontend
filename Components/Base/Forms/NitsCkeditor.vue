eb4243eb116f8447c824292b61f2447e33ff9b0a
Nitish Kumar <nitishkumar@noeticitservices.com>
2020-05-02 00:27:34 +0530

    CK Editor and dynamic routes bug fix

---
+++ Components/Base/Forms/NitsCkeditor.vue
@@ -0,0 +1,53 @@
+<template>
+    <ckeditor :editor="editor" :config="editorConfig"></ckeditor>
+</template>
+
+<script>
+    import CKEditor from '@ckeditor/ckeditor5-vue';
+    // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+
+    import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+    import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
+    import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
+    import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
+    import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
+
+    export default {
+        name: "NitsCkeditor",
+        components: {
+            // Use the <ckeditor> component in this view.
+            ckeditor: CKEditor.component
+        },
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorConfig: {
+                    plugins: [
+                        EssentialsPlugin,
+                        BoldPlugin,
+                        ItalicPlugin,
+                        LinkPlugin,
+                        ParagraphPlugin
+                    ],
+
+                    toolbar: {
+                        items: [
+                            'bold',
+                            'italic',
+                            'link',
+                            'undo',
+                            'redo'
+                        ]
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
