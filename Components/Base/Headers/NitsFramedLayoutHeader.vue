85be533abdf2e74fa17ecea7d8b9396f0bae7a84
nirmala <nirmala.suwasiya@gmail.com>
2020-05-05 20:13:54 +0530

    headers

---
+++ Components/Base/Headers/NitsFramedLayoutHeader.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class=" h-screen w-screen">
+<!--        <div class="absolute right-0 px-40 mt-64 -mr-20 ">-->
+<!--            <h3 class=" font-black text-4xl mt-40 text-white ">WELCOME TO NITSEDITOR</h3>-->
+<!--        </div>-->
+        <div class=" h-full w-full">
+            <img src="/project-assets/images/Regulatory-Compliance-Work-Blog.jpg" class="w-screen h-full" alt=" image">
+        </div>
+        <div class="absolute top-0 bg-transparent">
+                <nav class=" px-8 mx-auto w-screen ">
+                    <div class="flex justify-between h-32 ">
+                        <div class="float-left pr-3 ">
+                            <img class="h-16 w-56 mt-8 mr-5" src="/nits-assets/images/nits-editor-logo.png">
+                        </div>
+
+                        <div class="ml-64 mt-12 font-semibold">
+                            <div class="float-right ml-20">
+                                <div class="inline-block  text-white p-1  mx-4">
+                                    <a class="text-base " href="#">HOME</a>
+                                </div>
+                                <div class="inline-block text-white p-1 mx-4">
+                                    <a class="text-base" href="#">ABOUT</a>
+                                </div>
+                                <div class="inline-block text-white p-1 mx-4">
+                                    <a class="text-base " href="#">WORKS</a>
+                                </div>
+                                <div class="inline-block text-white  p-1 mx-4">
+                                    <a class="text-base " href="#">SERVICES</a>
+                                </div>
+                                <div class="inline-block text-white p-1 mx-4">
+                                    <a class="text-base " href="#">BLOG</a>
+                                </div>
+                                <div class="inline-block text-white  p-1 mx-4">
+                                    <a class="text-base " href="#">CONTACT</a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="mr-24 mt-2">
+                            <svg class="w-5 h-5 mt-12 cursor-pointer text-white mx-10" fill="currentColor"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M17 3a2 2 0 100 4 2 2 0 000-4zm-4.5 1h-11a.5.5 0 00-.5.5v1c0 .28.22.5.5.5h11a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5zm0 5h-11a.5.5 0 00-.5.5v1c0 .28.22.5.5.5h11a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5zm0 5h-11a.5.5 0 00-.5.5v1c0 .28.22.5.5.5h11a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5z"/>
+                            </svg>
+                        </div>
+                    </div>
+
+                </nav>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "NitsFramedLayoutHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
