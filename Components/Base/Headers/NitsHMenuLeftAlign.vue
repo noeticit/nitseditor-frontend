b8232baa3082eb2f7422577095f7493cb2f75d33
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-05-06 13:23:55 +0530

    somechanges

--- Components/Base/Headers/NitsHMenuLeftAlign.vue
+++ Components/Base/Headers/NitsHMenuLeftAlign.vue
@@ -3,12 +3,12 @@
         <div class="relative">
             <img class="w-screen h-screen" src="/nits-assets/images/joel.jpg">
         </div>
-        <div class="flex absolute top-0 justify-between items-center py-4 bg-transparent">
+        <div class="flex fixed top-0 justify-between items-center py-4 bg-transparent">
             <div class="flex-shrink-0 ml-8 cursor-pointer">
                 <img class="h-12 w-48" src="/nits-assets/images/nits-editor-logo.png">
             </div>
             <div class="ml-16 pl-5">
-                <ul class="hidden md:flex overflow-x-hidden uppercase ">
+                <ul class="hidden md:flex overflow-x-hidden uppercase">
                     <li class="mr-8 p-1 border-b-2 border-orange-500">
                         <a class="text-blue-800 tracking-widest antialiased text-xs cursor-default font-medium" href="#">Home</a>
                     </li>
@@ -34,10 +34,15 @@
                     </li>
                 </ul>
             </div>
-            <div class="absolute top-0 w-full " v-if="box">
+            <div class="absolute flex top-0 w-full " v-if="box">
                 <input class="w-full h-20 ml-2 border border-gray-500 mt-4 py-1 px-10 bg-gray-100 rounded placeholder-teal-600 text-sm focus:outline-none antialiased tracking-wide font-medium text-teal-600 leading-norma" type="text" placeholder="Search">
+
+                <svg @click="box = ! box" class="h-5 w-5 mt-12 ml-4 text-black hover:text-red-700" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M16 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2.939 12.789L10 11.729l-3.061 3.06-1.729-1.728L8.271 10l-3.06-3.061L6.94 5.21 10 8.271l3.059-3.061 1.729 1.729L11.729 10l3.06 3.061-1.728 1.728z"/>
+                </svg>
             </div>
         </div>
+        <div class="h-screen w-screen"></div>
 
     </div>
     
