61a05ecb6c71af2cbfa123e0188b55c3157389af
Nitish Kumar <nitishkumar@noeticitservices.com>
2020-05-09 13:23:41 +0530

    Exporting auth token

--- Models/_config.js
+++ Models/_config.js
@@ -1,15 +1,16 @@
-import {decrypt} from './_encrypt.js'
+import {decrypt} from './_encrypt.js';
 import VueSession from './_session';
 
 const session = new VueSession(process.env.MIX_STORAGE_PERSIST, process.env.MIX_INACTIVITY_SESSION);
 
+const tokenData = session.get('auth_user');
+
 export const getHeader = function () {
-    const tokenData = session.get('auth_user')
     const headers = {
         'Accept': 'application/json',
         'Authorization': tokenData ? 'Bearer ' + decrypt(tokenData.access_token) : null
     };
-    return headers
+    return headers;
 };
 
 export const clientId = process.env.MIX_CLIENT_ID;
@@ -19,3 +20,5 @@ export const clientSecret = process.env.MIX_CLIENT_SECRET;
 export const appUrl = process.env.MIX_APP_URL;
 
 export const appResource = 'check';
+
+export const accessToken = tokenData ? decrypt(tokenData.access_token) : null;
