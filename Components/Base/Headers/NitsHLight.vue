3e3d2abc80313762cc1092903a2e8fdd476a2137
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-05-11 15:22:29 +0530

    some changes

--- Components/Base/Headers/NitsHLight.vue
+++ Components/Base/Headers/NitsHLight.vue
@@ -10,9 +10,6 @@
                         <div class="relative inline-block" @mouseover="isVisible = true" @mouseleave="isVisible = false" @keydown.enter="isVisible = !isVisible">
                             <div class="inline-flex items-center justify-between px-2 py-1 font-medium  transition-all duration-500 rounded-md focus:outline-none focus:text-brand-900 sm:focus:shadow-outline">
                                 <span class="flex-shrink-0 text-sm">{{label1}}</span>
-<!--                                <svg fill="currentColor" viewBox="0 0 20 20" class="flex-shrink-0 w-5 h-5 ml-1">-->
-<!--                                    <path :class="{ 'rotate-180': isVisible }" class="transition duration-300 ease-in-out origin-center transform" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>-->
-<!--                                </svg>-->
                             </div>
                             <transition enter-active-class="transition duration-300 ease-out transform" enter-class="-translate-y-3 scale-95 opacity-0" enter-to-class="translate-y-0 scale-100 opacity-100" leave-active-class="transition duration-150 ease-in transform" leave-class="translate-y-0 opacity-100" leave-to-class="-translate-y-3 opacity-0">
                                 <div v-show="isVisible" class="absolute pt-2">
@@ -72,11 +69,14 @@
                         <path fill-rule="evenodd" d="M17.545 15.467l-3.779-3.779a6.15 6.15 0 00.898-3.21c0-3.417-2.961-6.377-6.378-6.377A6.185 6.185 0 002.1 8.287c0 3.416 2.961 6.377 6.377 6.377a6.15 6.15 0 003.115-.844l3.799 3.801a.953.953 0 001.346 0l.943-.943c.371-.371.236-.84-.135-1.211zM4.004 8.287a4.282 4.282 0 014.282-4.283c2.366 0 4.474 2.107 4.474 4.474a4.284 4.284 0 01-4.283 4.283c-2.366-.001-4.473-2.109-4.473-4.474z"/>
                     </svg>
                 </div>
-                <div class="mt-2">
-                    <nits-toggle-button
-                            :mode="mode"
-                            @toggle="$emit('toggle')"
-                    />
+                <div class="mt-3 ">
+                    <div class="pr-10">
+                        <nits-toggle-button
+                                :mode="mode"
+                                @toggle="$emit('toggle')"
+                        />
+                    </div>
+
                 </div>
             </div>
             <div class="pl-32 pr-10 ml-48" v-if="box">
