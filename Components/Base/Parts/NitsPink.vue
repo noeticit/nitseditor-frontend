763bb813c699bda18fc690b8b158731705257a73
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-05-16 00:14:28 +0530

    somechanges

---
+++ Components/Base/Parts/NitsPink.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="h-auto px-16 grid grid-cols-4 py-5 bg-pink-600 gap-4">
+        <div class="bg-transparent  mt-5 pb-6 justify-center">
+            <div class="flex justify-between text-center items-center px-8 py-5">
+                <div class="mb-16">
+                    <svg class="h-12 mb-8 w-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M14.004 0H5.996A1.996 1.996 0 004 1.996v16.007C4 19.106 4.894 20 5.996 20h8.007A1.997 1.997 0 0016 18.004V1.996A1.996 1.996 0 0014.004 0zM10 19c-.69 0-1.25-.447-1.25-1s.56-1 1.25-1 1.25.447 1.25 1-.56 1-1.25 1zm4-3H6V2h8v14z"/>
+                    </svg>
+                </div>
+                <div class="flex-col">
+                    <h3 class="text-left text-xl ml-5 text-white tracking-normal font-mono leading-snug antialiased mt-5 font-bold">POWERFUL LMS ON VUE.JS</h3>
+                    <p class="text-left text-white text-base ml-2 antialiased leading-snug tracking-normal  mt-2 p-3   antialiased mb-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis,
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="bg-transparent  mt-5  justify-center">
+            <div class="flex justify-between text-center items-center px-8 py-5">
+                <div class="mb-16">
+                    <svg class="h-12 mb-8 w-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M13.648 7.362c-.133-.355 3.539-3.634 1.398-6.291-.501-.621-2.201 2.975-4.615 4.603C9.099 6.572 6 8.484 6 9.541v6.842C6 17.654 10.914 19 14.648 19 16.017 19 18 10.424 18 9.062c0-1.368-4.221-1.344-4.352-1.7zM5 7.457c-.658 0-3 .4-3 3.123v4.848c0 2.721 2.342 3.021 3 3.021.657 0-1-.572-1-2.26V9.816c0-1.768 1.657-2.359 1-2.359z"/>
+                    </svg>
+                </div>
+                <div class="flex-col">
+                    <h3 class="text-left text-xl font-mono ml-5 text-white tracking-normal leading-snug antialiased mt-5 font-bold">PEFFORTLESSLY MANAGE COURSES</h3>
+                    <p class="text-left text-white text-base ml-2 antialiased leading-snug tracking-normal  mt-2 p-3   antialiased mb-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis,
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-transparent  mt-5  justify-center">
+            <div class="flex justify-between text-center items-center px-8 py-5">
+                <div class="mb-16">
+                    <svg class="h-12 mb-8 w-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M16.783 10c0-1.049.646-1.875 1.617-2.443a8.932 8.932 0 00-.692-1.672c-1.089.285-1.97-.141-2.711-.883-.741-.74-.968-1.621-.683-2.711a8.732 8.732 0 00-1.672-.691c-.568.97-1.595 1.615-2.642 1.615-1.048 0-2.074-.645-2.643-1.615a8.697 8.697 0 00-1.671.691c.285 1.09.059 1.971-.684 2.711-.74.742-1.621 1.168-2.711.883A8.797 8.797 0 001.6 7.557c.97.568 1.615 1.394 1.615 2.443 0 1.047-.645 2.074-1.615 2.643a8.89 8.89 0 00.691 1.672c1.09-.285 1.971-.059 2.711.682.741.742.969 1.623.684 2.711a8.841 8.841 0 001.672.693c.568-.973 1.595-1.617 2.643-1.617 1.047 0 2.074.645 2.643 1.617a8.963 8.963 0 001.672-.693c-.285-1.088-.059-1.969.683-2.711.741-.74 1.622-1.166 2.711-.883a8.811 8.811 0 00.692-1.672c-.973-.569-1.619-1.395-1.619-2.442zM10 13.652a3.652 3.652 0 110-7.306 3.653 3.653 0 010 7.306z"/>
+                    </svg>
+                </div>
+                <div class="flex-col">
+                    <h3 class="text-left text-xl ml-5 text-white font-mono tracking-normal leading-snug antialiased mt-5 font-bold">EASILY SELL COURSES ONLINE</h3>
+                    <p class="text-left text-white text-base ml-2 antialiased leading-snug tracking-normal  mt-2 p-3   antialiased mb-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis,
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="bg-transparent  mt-5  justify-center">
+            <div class="flex justify-between text-center items-center px-8 py-5">
+                <div class="mb-16">
+                    <svg class="h-12 mb-8 w-12 text-yellow-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M5.8 12.2V6H2C.9 6 0 6.9 0 8v6c0 1.1.9 2 2 2h1v3l3-3h5c1.1 0 2-.9 2-2v-1.82a.943.943 0 01-.2.021h-7V12.2zM18 1H9c-1.1 0-2 .9-2 2v8h7l3 3v-3h1c1.1 0 2-.899 2-2V3c0-1.1-.9-2-2-2z"/>
+                    </svg>
+                </div>
+                <div class="flex-col">
+                    <h3 class="text-left text-xl ml-5 text-white font-mono tracking-normal leading-snug antialiased mt-5 font-bold">24/7 SUPPORT WITH LIVE CHAT</h3>
+                    <p class="text-left text-white text-base ml-2 antialiased leading-snug tracking-normal  mt-2 p-3   antialiased mb-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis,
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NitsPink"
+    }
+</script>
+
+<style scoped>
+
+</style>
