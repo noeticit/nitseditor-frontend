ef8efaea4937476bea0d7b1c62ac4d09e453b638
nirmala <nirmala.suwasiya@gmail.com>
2020-05-16 21:02:08 +0530

    confidence UI

--- Components/Base/Parts/NitsCourseSelection.vue
+++ Components/Base/Parts/NitsCourseSelection.vue
@@ -2,16 +2,16 @@
     <div>
         <div class="flex w-full">
             <div class=" w-2/5 h-2/5 mt-20 ml-32">
-                <h1 class="text-4xl text-gray-700 font-bold  py-8 pb-4 text-blue-600 font-bold">COURSE CREATION</h1>
+                <h1 class="text-4xl font-bold  py-8 pb-4 text-blue-600 font-bold">COURSE CREATION</h1>
                 <p class="text-2xl leading-normal text-gray-600">
                     It has never been easier! No need to type any line of code. Building courses is as easy as breathing
                     if you have a magic <b class="font-semibold text-gray-700">Front-end course builder</b>. Super convenient and transparent builder,
                     visual composer plugin and functional drag and drop tools - everything at hand to make it as easy as possible.
                 </p>
-                <button class="text-xl py-3 px-5 mt-6 text-white bg-blue-700 shadow-md cursor-pointer border-2 rounded-full ">Live Preview</button>
+                <button class="text-xl py-3 px-5 mt-6 text-white bg-blue-700 shadow-md cursor-pointer border-2 rounded-lg ">Get Started</button>
             </div>
             <div class="w-2/4 mt-12 mr-24">
-                <img class=" h-2/3" src="/project-assets/images/undraw_collaborators_prrw.png" alt="assignment">
+                <img class=" h-2/3" src="/project-assets/images/undraw_content_creator_xeju.png" alt="course selection">
             </div>
 
         </div>
