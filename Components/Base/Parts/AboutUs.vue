2eb6ad1054c2620b92728f373ab4ee125a08cf9a
Pooja Bisht <poojabisht2604@gmail.com>
2020-05-16 20:24:59 +0530

    website

---
+++ Components/Base/Parts/AboutUs.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <div class="relative ">
+        <img  src="/nits-assets/images/banner.jpg" alt="" class=" w-full">
+        <div class="w-full h-64 absolute top-0 mt-20 ">
+            <h2 class="uppercase content-center text-center  text-white font-bold text-4xl mt-2 ">
+            About Us
+        </h2>
+            <p class=" content-center text-center mt-2 text-white font-semibold text-lg ">
+                <a  href="" class=" content-center text-center mt-2 text-white font-semibold text-lg cursor-pointer">
+            Home
+        </a>/<a href="" class="cursor-pointer">About us</a></p></div>
+    </div>
+    <section class="py-8">
+        <div class="container  mx-auto flex flex-wrap flex-col items-center md:flex-row mt-8">
+            <div class="flex flex-col w-full md:w-5/12 justify-center items-start mx-auto ">
+                <h1 class="my-4 text-4xl font-bold leading-tight text-gray-800 ">ABOUT CONFIDENCE EDUCARE</h1>
+                <p class="text-xl leading-normal text-black text-gray-600">Confidence Educare is a team of experienced and dedicated teachers with over a decade’s experience in the field of imparting knowledge through conducting Group Tuitions, providing printed notes, e-learning, paper setting and assessment of answer papers by conducting test series in commerce stream for junior college and degree section.
+                   </p>
+            </div>
+
+            <div class="flex justify-center h-25 md:w-5/12 mr-10">
+                <img class="w-full md:w-5/7 py-8 px-8 bg-contain " src="/nits-assets/images/usernames.svg">
+            </div>
+        </div>
+        <div class="container  mx-auto flex flex-wrap flex-col items-center md:flex-row mt-8">
+            <div class="flex flex-col w-full md:w-5/12 justify-center items-start ml-24 ">
+                <h1 class="my-4 text-2xl font-bold leading-tight text-gray-700 ">CONFIDENCE EDUCARE <h2 class="text-blue-600">IS THE BEST !</h2></h1>
+                <p class="text-xl leading-normal text-black text-gray-600">Confidence Educare is a team of experienced and dedicated teachers with over a decade’s experience in the field of imparting knowledge through conducting Group Tuitions, providing printed notes, e-learning, paper setting and assessment of answer papers by conducting test series in commerce stream for junior college and degree section.
+                </p>
+            </div>
+
+<!--            <div class="flex justify-center h-25 md:w-5/12 mr-10">-->
+                <div class="flex flex-col w-full md:w-5/12 justify-center items-start ml-24 ">
+                    <h1 class="my-4 text-2xl font-bold leading-tight text-gray-700 uppercase">CONFIDENCE EDUCARE <h2 class="text-blue-600">Live lectures</h2></h1>
+                    <p class="text-xl leading-normal text-black text-gray-600">Confidence Educare is a team of experienced and dedicated teachers with over a decade’s experience in the field of imparting knowledge through conducting Group Tuitions, providing printed notes, e-learning, paper setting and assessment of answer papers by conducting test series in commerce stream for junior college and degree section.
+                    </p>
+                </div>
+
+
+<!--            </div>-->
+        </div>
+
+    </section>
+
+</div>
+</template>
+
+<script>
+    export default {
+        name: "AboutUs"
+    }
+</script>
+
+<style scoped>
+
+</style>
