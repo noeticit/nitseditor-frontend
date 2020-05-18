048fd4bf1be068c046862d4f6c8cc73ddd77b12f
nirmala <nirmala.suwasiya@gmail.com>
2020-05-18 20:22:30 +0530

    confidence UI changes

--- Components/Base/Parts/NitsWelcomeConfidence.vue
+++ Components/Base/Parts/NitsWelcomeConfidence.vue
@@ -9,7 +9,7 @@
                     imparting knowledge through conducting Group Tuitions, providing printed notes, e-learning, paper setting and
                     assessment of answer papers by conducting test series in commerce stream for junior college and degree section.
                 </p>
-                <button class="text-xl py-3 px-5 mt-6 text-white bg-blue-700 shadow-md cursor-pointer border-2 rounded-lg ">Get Started</button>
+                <button class="text-xl py-3 px-5 mt-6 text-white bg-blue-700 shadow-md focus:outline-none cursor-pointer border-2 rounded-lg ">Get Started</button>
             </div>
             <div class="w-2/4 mt-56 ml-24">
                 <img class=" h-2/3" src="/nits-assets/images/students-2.png" alt="students">
