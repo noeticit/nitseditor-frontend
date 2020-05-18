fccc5ebbe8729bde904e725d5a1367502ead3432
Pooja Bisht <poojabisht2604@gmail.com>
2020-05-18 22:48:53 +0530

    changes

--- Pages/themes.vue
+++ Pages/themes.vue
@@ -1,20 +1,31 @@
 <template>
-    <dashboard-layout-one active="themes">
-        <base-bread-crumb-one name="Themes" :breadcrumbs="breadcrumbs"></base-bread-crumb-one>
-    </dashboard-layout-one>
+
+    <div>
+    <div>
+        <nits-filter-courses></nits-filter-courses>
+    </div>
+    <div>
+        <live-streaming></live-streaming>
+    </div>
+        <div>
+            <services></services>
+        </div>
+        <div>
+            <about-us></about-us>
+        </div>
+    </div>
 </template>
 
 <script>
+
     export default {
         name: "themes",
+
         data() {
             return {
-                breadcrumbs: ['Themes', 'Second Link', 'Third Link', 'Fourth Link'],
             }
         },
-        components: {
-            'DashboardLayoutOne': () => import('./../Layouts/Dashboard/LayoutOne'),
-        }
+
     }
 </script>
 
