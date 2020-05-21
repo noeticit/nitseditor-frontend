486d177157872b0f22c5f9734d7bc4ecd56e2c17
nirmala <nirmala.suwasiya@gmail.com>
2020-05-21 13:45:43 +0530

    changes in header

--- Components/Base/Headers/NitsHeaderStaticSticky.vue
+++ Components/Base/Headers/NitsHeaderStaticSticky.vue
@@ -1,107 +1,48 @@
 <template>
     <div class="">
         <nav class="sticky top-0 bg-white px-8 mx-auto border-b w-screen ">
-            <div class="flex justify-between h-32 ">
+            <div class="flex justify-between h-24 ">
                 <div class="float-left border-r pr-3 ">
-                    <img class="h-16 w-56 mt-8 mr-5" src="/nits-assets/images/nits-editor-logo.png">
+                    <img class="h-16 w-56 mt-4 mr-5" src="/nits-assets/images/nits-editor-logo.png">
                 </div>
 
-                <div class=" ml-56 font-semibold">
-                    <div class="float-right ml-12">
-                        <div class="inline-block  text-gray-800 p-1 mx-2">
-                          <a class="text-lg hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" @mouseover="isVisibleHeader1 = true" @mouseleave="isVisibleHeader1 = false"  @keydown.enter="isVisibleHeader1 = !isVisibleHeader1" href="#">
-                                {{label1}}
+<!--                <div class=" ml-56 font-semibold">-->
+                    <div class="float-right ml-56">
+                        <div v-for="item in menu" class="inline-block  text-gray-800 p-1 mx-2">
+                          <a class="text-sm hover:text-teal-600 font-semibold mt-0 lg:inline-block lg:mt-0 lg:py-8 px-2" @mouseover="isVisibleHeader1 = true" @mouseleave="isVisibleHeader1 = false"  @keydown.enter="isVisibleHeader1 = !isVisibleHeader1" href="#">
+                                {{item}}
 <!--                                HOME-->
-                              <ul v-show="isVisibleHeader1" class=" absolute top-0 mt-32 mr-6 w-56 text-gray-600 bg-white border-t-2 border-teal-400" >
-                                  <li class=""  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = ! isVisible1">
-                                      <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                            <span class="block text-base" >HOME-CORPORATE</span>
-                                            <span class="mr-auto">
-                                              <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                            </svg>
-                                            </span>
-                                        </a>
-                                    </li>
-                               <li class=""  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = ! isVisible1">
-                                   <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                       <span class="flex text-base hover:text-teal-500" >HOME-PORFOLIO</span>
-                                       <span class="mr-auto">
-                                            <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                              <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                            </svg>
-                                        </span>
-                                   </a>
-                               </li>
+<!--                              <ul v-show="isVisibleHeader1" class=" absolute top-0 mt-32 mr-6 w-56 text-gray-600 bg-white border-t-2 border-teal-400" >-->
+<!--                                  <li class=""  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = ! isVisible1">-->
+<!--                                      <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">-->
+<!--                                            <span class="block text-base" >HOME-CORPORATE</span>-->
+<!--                                            <span class="mr-auto">-->
+<!--                                              <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">-->
+<!--                                                <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>-->
+<!--                                            </svg>-->
+<!--                                            </span>-->
+<!--                                        </a>-->
+<!--                                    </li>-->
+<!--                               <li class=""  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = ! isVisible1">-->
+<!--                                   <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">-->
+<!--                                       <span class="flex text-base hover:text-teal-500" >HOME-PORFOLIO</span>-->
+<!--                                       <span class="mr-auto">-->
+<!--                                            <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">-->
+<!--                                              <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>-->
+<!--                                            </svg>-->
+<!--                                        </span>-->
+<!--                                   </a>-->
+<!--                               </li>-->
 
-                           </ul>
+<!--                           </ul>-->
 
                           </a>
                         </div>
-                        <div class="inline-block  p-1 mx-2">
-                            <a class="text-lg text-teal-500 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" @mouseover="isVisibleHeader2 = true"  @mouseleave="isVisibleHeader2 = false" @keydown.enter="isVisibleHeader2 = !isVisibleHeader2" href="#">
-                                {{label2}}
-<!--                                FEATURES-->
-                                <ul v-show="isVisibleHeader2" class=" absolute top-0 mt-32 mr-6 w-56 text-gray-600 bg-white border-t-2 border-teal-400" >
-                                    <li class=""  @mouseover="isVisible2 = true" @mouseleave="isVisible2 = false" @keydown.enter="isVisible2 = !isVisible2">
-                                        <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                            <span class="block text-base" >SLIDERS</span>
-                                            <span class="mr-auto">
-                                              <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                            </svg>
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li class=""  @mouseover="isVisible2 = true" @mouseleave="isVisible2 = false" @keydown.enter="isVisible2 = !isVisible2">
-                                        <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                            <span class="flex text-base hover:text-teal-500" >WIDGETS</span>
-                                            <span class="mr-auto">
-                                            <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                              <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                            </svg>
-                                        </span>
-                                        </a>
-                                    </li>
 
-                                </ul>
-
-                            </a>
-                        </div>
-                        <div class="inline-block text-gray-800 p-1 mx-2">
-                            <a class="text-lg hover:text-teal-500 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" href="#">
-                                {{label3}}
-<!--                            PAGES-->
-                            </a>
-                        </div>
-                        <div class="inline-block text-gray-800  p-1 mx-2">
-                            <a class="text-lg hover:text-teal-500 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" href="#">
-                                {{label4}}
-<!--                            PORTFOLIO-->
-                            </a>
-                        </div>
-                        <div class="inline-block text-gray-800 p-1 mx-2">
-                            <a class="text-lg hover:text-teal-500 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" href="#">
-                                {{label5}}
-<!--                            BLOG-->
-                            </a>
-                        </div>
-                        <div class="inline-block text-gray-800  p-1 mx-2">
-                            <a class="text-lg hover:text-teal-500 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" href="#">
-                                {{label6}}
-<!--                           SHOP-->
-                            </a>
-                        </div>
-                        <div class="inline-block text-gray-800 p-1 mx-2">
-                            <a class="text-lg hover:text-teal-500 font-semibold mt-4 lg:inline-block lg:mt-0 lg:py-12 px-2" href="#">
-                                {{label7}}
-<!--                            SHORTCODES-->
-                            </a>
-                        </div>
                     </div>
-                </div>
-                <div @click="box = ! box" class="border-l ">
-                    <svg class="w-5 h-5 mt-12 cursor-pointer text-gray-800 mx-10" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+<!--                </div>-->
+                <div @click="search = ! search" class="border-l ">
+                    <svg class="w-5 h-5 mt-10 cursor-pointer text-gray-800 mx-10" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M17.545 15.467l-3.779-3.779a6.15 6.15 0 00.898-3.21c0-3.417-2.961-6.377-6.378-6.377A6.185 6.185 0 002.1 8.287c0 3.416 2.961 6.377 6.377 6.377a6.15 6.15 0 003.115-.844l3.799 3.801a.953.953 0 001.346 0l.943-.943c.371-.371.236-.84-.135-1.211zM4.004 8.287a4.282 4.282 0 014.282-4.283c2.366 0 4.474 2.107 4.474 4.474a4.284 4.284 0 01-4.283 4.283c-2.366-.001-4.473-2.109-4.473-4.474z"/>
                     </svg>
                 </div>
@@ -109,10 +50,10 @@
 
         </nav>
 
-        <div class="absolute flex top-0 w-full " v-if="box">
-            <input class="w-full h-32 ml-2 border border-gray-300 py-1 px-10 bg-white rounded placeholder-gray-700 text-xl focus:outline-none antialiased tracking-wide font-medium text-gray-700 leading-normal" type="text" placeholder="TYPE HERE..">
+        <div class="absolute flex top-0 w-full " v-if="search">
+            <input class="w-full h-24 ml-2 border border-gray-300 py-1 px-10 bg-white rounded placeholder-gray-700 text-xl focus:outline-none antialiased tracking-wide font-medium text-gray-700 leading-normal" type="text" placeholder="TYPE HERE..">
 
-            <svg @click="box = ! box" class="h-5 w-5 mt-12 ml-4 text-black " xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <svg @click="search = ! search" class="h-5 w-5 mt-8 ml-4 text-black " xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd" d="M16 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2.939 12.789L10 11.729l-3.061 3.06-1.729-1.728L8.271 10l-3.06-3.061L6.94 5.21 10 8.271l3.059-3.061 1.729 1.729L11.729 10l3.06 3.061-1.728 1.728z"/>
             </svg>
         </div>
@@ -133,44 +74,18 @@
             return{
                 isVisibleHeader1: false,
                 isVisibleHeader2: false,
-                box: false,
+                search: false,
 
             }
         },
 
         props: {
-            label1: {
-                type: String,
-                required: true
-            },
-            label2: {
-                type: String,
-                required: true
-            },
-            label3: {
-                type: String,
-                required: true
-            },
-            label4: {
-                type: String,
-                required: true
-            },
-            label5: {
-                type: String,
-                required: true
-            },
-            label6: {
-                type: String,
-                required: true
-            },
-            label7: {
-                type: String,
-                required: true
-            },
+
             heading:{
                 type: String,
                 required: true
-            }
+            },
+            menu: Array
         },
 
     }
--- Components/Base/Headers/NitsHeaderStickyLogoChange.vue
+++ Components/Base/Headers/NitsHeaderStickyLogoChange.vue
@@ -2,110 +2,57 @@
    <div class="relative">
 <!--      <img src="/project-assets/images/HiRes1-150x150.jpg.jpg" class="h-full w-screen" alt=" image">-->
 
-       <nav  class="sticky top-0 bg-white flex w-screen h-32 ">
+       <nav  class="sticky top-0 bg-white flex w-screen h-24 ">
         <div class="flex justify-between">
-          <div class="float-left ml-20 mt-6 ">
-            <img src="/nits-assets/images/nits-editor-logo.png" alt="logo" class="h-20">
+          <div class="float-left ml-4 mt-4 ">
+            <img src="/nits-assets/images/nits-editor-logo.png" alt="logo" class="h-16">
           </div>
 
-          <div class=" flex items-center ml-4" >
+<!--          <div class=" flex items-center ml-4" >-->
             <div class="float-right ml-64 mt-1">
 <!--                <div class="inline-block pl-4 mx-5 " >-->
-                    <ul class="lg:flex-grow font-medium uppercase flex flex-col lg:flex-row pb-4 lg:pb-0 text-gray-700">
-                        <li class="hover:text-teal-400"  @mouseover="isVisibleHeader1 = true" @mouseleave="isVisibleHeader1 = false" @keydown.enter="isVisibleHeader1 = !isVisibleHeader1">
-                           <a class="text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2"  href="">
-                          {{label1}}
+                    <ul class="lg:flex-grow font-medium uppercase flex flex-col lg:flex-row pb-4 lg:pb-0 text-sm  text-gray-700">
+                        <li class="hover:text-teal-400" v-for="item in menu">
+<!--                            @mouseover="isVisibleHeader1 = true" @mouseleave="isVisibleHeader1 = false" @keydown.enter="isVisibleHeader1 = !isVisibleHeader1">-->
+                           <a class=" font-semibold lg:inline-block lg:mt-0 mx-3 lg:py-8 px-2"  href="">
+                          {{item}}
  <!--                        HOME-->
                            </a>
-                          <ul v-show="isVisibleHeader1" class="dropdown-menu absolute w-56 text-gray-600 bg-white border-t-2 border-teal-400" >
-                          <li class=" hover:text-teal-500"  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = !isVisible1">
-                             <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                             <span class="block" >HOME-CORPORATE</span>
-                             <span class="mr-auto">
-                                  <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                </svg>
-                             </span>
-                                    </a>
-                                </li>
+<!--                          <ul v-show="isVisibleHeader1" class="dropdown-menu absolute w-56 text-gray-600 bg-white border-t-2 border-teal-400" >-->
+<!--                          <li class=" hover:text-teal-500"  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = !isVisible1">-->
+<!--                             <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">-->
+<!--                             <span class="block" >HOME-CORPORATE</span>-->
+<!--                             <span class="mr-auto">-->
+<!--                                  <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">-->
+<!--                                    <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>-->
+<!--                                </svg>-->
+<!--                             </span>-->
+<!--                                    </a>-->
+<!--                                </li>-->
 
-                                <li class=""  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = !isVisible1">
-                                    <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                        <span class="flex  hover:text-teal-500" >HOME-PORFOLIO</span>
-                                        <span class="mr-auto">
-                                            <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                              <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                            </svg>
-                                        </span>
-                                    </a>
-                                </li>
+<!--                                <li class=""  @mouseover="isVisible1 = true" @mouseleave="isVisible1 = false" @keydown.enter="isVisible1 = !isVisible1">-->
+<!--                                    <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">-->
+<!--                                        <span class="flex  hover:text-teal-500" >HOME-PORFOLIO</span>-->
+<!--                                        <span class="mr-auto">-->
+<!--                                            <svg class="absolute right-0 w-5 h-5  ml-40 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">-->
+<!--                                              <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>-->
+<!--                                            </svg>-->
+<!--                                        </span>-->
+<!--                                    </a>-->
+<!--                                </li>-->
 
-                            </ul>
+<!--                            </ul>-->
                         </li>
 <!--                </div>-->
-                <li class="inline-block pl-2 ">
-                    <a class=" text-gray-700 hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2"  @mouseover="isVisibleHeader2 = true" @mouseleave="isVisibleHeader2 = false" @keydown.enter="isVisibleHeader2 = !isVisibleHeader2" href="">
-                      {{label2}}
-<!--                   FEATURE-->
-                       <ul class="dropdown-menu absolute mt-12 w-40 mr-2 text-gray-600 bg-white border-t-2 border-teal-400" v-show="isVisibleHeader2">
-                                <li class="text-teal-500 hover:text-teal-500"  @mouseover="isVisible2 = true" @mouseleave="isVisible2 = false" @keydown.enter="isVisible2 = !isVisible2" >
-                                  <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                    <span class="flex " >SLIDERS</span>
-                                      <span class="mr-auto">
-                                  <svg class="absolute right-0 w-5 h-5  ml-32 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                </svg>
-                             </span>
-
-                                   </a>
-                                </li>
-                                <li class="">
-                                    <a class="rounded-t  hover:bg-gray-100 py-4 px-4 block whitespace-no-wrap flex " href="#">
-                                    <span class="flex  hover:text-teal-500" >WIDGETS</span>
-                                        <span class="mr-auto">
-                                            <svg class="absolute right-0 w-5 h-5 ml-32 " fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                              <path fill-rule="evenodd" d="M11 10L7.86 6.58a.7.7 0 010-.98c.27-.27.7-.27.97 0l3.83 3.91a.7.7 0 010 .98l-3.83 3.9a.68.68 0 01-.97 0 .7.7 0 010-.97L11 10z"/>
-                                            </svg>
-                                        </span>
-                                    </a>
-                                </li>
-                            </ul>
-                    </a>
-                </li>
-                <li class="inline-block pl-2 ">
-                    <a class=" text-gray-700 hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2" href="">{{label3}}
-<!--                        PAGES-->
-                    </a>
-                </li>
-                <li class="inline-block pl-2 ">
-                    <a class=" text-gray-700 hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2" href="">{{label4}}
-<!--                        PORTFOLIO-->
-                    </a>
-                </li>
-                <li class="inline-block ">
-                    <a class=" text-gray-700 hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2" href="">{{label5}}
-<!--                        BLOG-->
-                    </a>
-                </li>
-                <li class="inline-block ">
-                    <a class=" text-gray-700 hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2" href="">{{label6}}
-<!--                        SHOP-->
-                    </a>
-                </li>
-                <li class="inline-block ">
-                    <a class=" text-gray-700 hover:text-teal-600 font-semibold mt-4 lg:inline-block lg:mt-0 mx-4 lg:py-12 px-2" href="">{{label7}}
-<!--                        SHORTCODES-->
-                    </a>
-                </li>
               </ul>
             </div>
 
-        </div>
+<!--        </div>-->
 
           <!--search icon-->
-        <div @click="box = ! box" class="flex absolute right-0 mt-12">
+        <div @click="search = ! search" class="flex absolute right-0 mt-8">
 <!--            <a  class=" py-2 px-4 mt-2 text-gray-700 text-lg font-semibold rounded-lg" href="">-->
-                <svg class="w-5 h-5 mx-24 mt-2" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <svg class="w-5 h-5 mx-24 mt-1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path fill-rule="evenodd" d="M17.55 15.47l-3.78-3.78c.57-.94.9-2.04.9-3.21A6.52 6.52 0 008.28 2.1 6.18 6.18 0 002.1 8.3a6.52 6.52 0 006.38 6.37c1.13 0 2.2-.3 3.11-.84l3.8 3.8c.37.37.98.37 1.35 0l.94-.94c.37-.37.24-.84-.13-1.21zM4 8.29A4.28 4.28 0 018.3 4c2.36 0 4.47 2.11 4.47 4.48a4.28 4.28 0 01-4.28 4.28A4.62 4.62 0 014 8.3z"/>
                 </svg>
 <!--            </a>-->
@@ -113,10 +60,10 @@
 
         </div>
     </nav>
-       <div class="absolute flex top-0 w-full " v-if="box">
-           <input class="w-full h-32 ml-2 border border-gray-300 py-1 px-10 bg-white rounded placeholder-gray-700 text-xl focus:outline-none antialiased tracking-wide font-medium text-gray-700 leading-normal" type="text" placeholder="TYPE HERE..">
+       <div class="absolute flex top-0 w-full " v-if="search">
+           <input class="w-full h-24 ml-2 border border-gray-300 py-1 px-10 bg-white rounded placeholder-gray-700 text-xl focus:outline-none antialiased tracking-wide font-medium text-gray-700 leading-normal" type="text" placeholder="TYPE HERE..">
 
-           <svg @click="box = ! box" class="h-5 w-5 mt-12 ml-4 text-black" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+           <svg @click="search = ! search" class="h-5 w-5 mt-8 ml-4 text-black" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                <path fill-rule="evenodd" d="M16 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2.939 12.789L10 11.729l-3.061 3.06-1.729-1.728L8.271 10l-3.06-3.061L6.94 5.21 10 8.271l3.059-3.061 1.729 1.729L11.729 10l3.06 3.061-1.728 1.728z"/>
            </svg>
        </div>
@@ -151,39 +98,13 @@
             return{
                 isVisibleHeader1: false,
                 isVisibleHeader2: false,
-                box:false,
+                search:false,
             }
         },
 
         props: {
-            label1: {
-                type: String,
-                required: true
-            },
-            label2: {
-                type: String,
-                required: true
-            },
-            label3: {
-                type: String,
-                required: true
-            },
-            label4: {
-                type: String,
-                required: true
-            },
-            label5: {
-                type: String,
-                required: true
-            },
-            label6: {
-                type: String,
-                required: true
-            },
-            label7: {
-                type: String,
-                required: true
-            },
+            menu: Array,
+
             heading:{
                 type: String,
                 required: true
