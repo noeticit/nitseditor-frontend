032a879d2fd81fd718ce3ea4dfbc98c15b58994d
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-05-23 12:32:12 +0530

    some changes In Menus

--- Components/Base/Menus/NitsSideMenus.vue
+++ Components/Base/Menus/NitsSideMenus.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="mt-10 px-8 font-bold">
         <div v-for="(item,index) in menus" class="mt-5 " @mouseover="Visible = index" @mouseleave="Visible = false" @keydown.enter="Visible = !Visible">
-            <router-link class="inline-flex items-center justify-between px-2 py-1 font-medium  transition-all duration-500 rounded-md focus:outline-none focus:text-brand-900 sm:focus:shadow-outline" :to="item.link">
-                <span class="flex-shrink-0 tracking-widest text-gray-700 text-sm uppercase">{{item.name}}</span>
-                <div class="ml-10">
-                    <svg fill="currentColor" viewBox="0 0 20 20" class="flex-shrink-0 w-5 h-5 text-gray-700 mr-8">
+            <router-link class="flex text-center items-center justify-between px-2 py-1 font-medium  transition-all duration-500 rounded-md focus:outline-none focus:text-brand-900 sm:focus:shadow-outline" :to="item.link">
+                <span class="text-left flex-shrink-0 tracking-widest text-gray-700 text-sm uppercase">{{item.name}}</span>
+                <div class=" flex">
+                    <svg fill="currentColor" viewBox="0 0 20 20" class="flex-shrink-0  w-5 h-5 text-gray-700">
                         <path :class="{ 'rotate-180': Visible === index}" class="transition duration-300 ease-in-out origin-center transform" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
                 </div>
@@ -20,6 +20,7 @@
                 </div>
             </transition>
         </div>
+
     </div>
 
 </template>
