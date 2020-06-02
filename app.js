ee321ab6d5160afe3ce4dfdde38e293eb82a12c1
Nitish Kumar <nitishkumar@noeticitservices.com>
2020-06-02 09:34:08 +0530

    Clean ups

--- app.js
+++ app.js
@@ -55,24 +55,6 @@ router.beforeEach((to, from, next) => {
         else
             next({path: '/nits-admin/not-subscribed'});
     }
-
-    // if(to.meta.requiresAuth && auth.isLoggedIn())
-    //     next();
-    // if(!to.meta.requiresAuth && auth.isLoggedIn())
-    //     next();
-    // if(to.meta.requiresAuth && !auth.isLoggedIn())
-    //     next ({path: '/'});
-        // var pathname=(window.location.pathname)  //      /admin/plugins/Biltrax/project-search
-
-        // console.log(window.location.pathname)
-        // var path = (window.location.pathname).replace('/nits-admin/', '/')     //      /plugins/Biltrax/project-search
-        // console.log(path)
-        // if(path.length>2) {
-        //     sessions.set('last_url', path);   // plugins/Biltrax/project-search
-        // }
-
-    // if(!to.meta.requiresAuth && !auth.isLoggedIn())
-    //     next()
 });
 
 /**
