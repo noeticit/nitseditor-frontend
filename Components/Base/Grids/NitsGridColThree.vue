cc78da0fb8c590af24dc0a6a353b56f56523a60b
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-06-03 14:50:02 +0530

    somechanges

--- Components/Base/Grids/NitsGridColThree.vue
+++ Components/Base/Grids/NitsGridColThree.vue
@@ -1,46 +1,35 @@
 <template>
     <div class="h-auto grid grid-cols-3 gap-4">
-        <div class="bg-white rounded-lg shadow-lg mt-5  justify-center">
-            <div class="flex justify-between border-solid border-b-2 border-teal-500 p-2 text-center items-center">
+        <div class="bg-white rounded-lg  mt-5 justify-center">
+            <div class="flex justify-between border-solid p-2 text-center items-center">
                 <div class="text-left font-semibold text-gray-700 ml-2 text-xl antialiased leading-snug tracking-normal font-sans">
                     {{heading1}} <span class="text-teal-500">{{subheading1}}</span>
                 </div>
-                <div class="relative">
-<!--                    <slot name="toolbox"></slot>-->
-                </div>
             </div>
-            <div :class="'mt-2 p-3 font-sans text-gray-700 antialiased mb-2'+height">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis, nam expedita, ea fuga commodi
-                voluptas iusto, hic autem deleniti dolores explicabo labore enim repellat earum perspiciatis.
+            <div class="relative">
+                <slot name="toolbox"></slot>
             </div>
         </div>
 
-        <div class="bg-white rounded-lg shadow-lg mt-5  justify-center">
-            <div class="flex justify-between border-solid border-b-2 border-teal-500 p-2 text-center items-center">
+        <div class="bg-white rounded-lg  mt-5  justify-center">
+            <div class="flex justify-between border-solid  p-2 text-center items-center">
                 <div class="text-left font-semibold text-gray-700 ml-2 text-xl antialiased leading-snug tracking-normal font-sans">
                     {{heading2}} <span class="text-teal-500">{{subheading2}}</span>
                 </div>
-                <div class="relative">
-                    <slot name="toolbox"></slot>
-                </div>
             </div>
-            <div :class="'mt-2 p-3 font-sans text-gray-700 antialiased mb-2'+height">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis, nam expedita, ea fuga commodi voluptas iusto, hic autem deleniti dolores explicabo labore enim repellat earum perspiciatis.
+            <div class="relative">
+                <slot name="toolbox1"></slot>
             </div>
         </div>
-        <div class="bg-white rounded-lg shadow-lg mt-5  justify-center">
-            <div class="flex justify-between border-solid border-teal-500 border-b-2 p-2 text-center items-center">
+        <div class="bg-white rounded-lg mt-5  justify-center">
+            <div class="flex justify-between border-solid  p-2 text-center items-center">
 
                 <div class="text-left font-semibold text-gray-700 ml-2 text-xl antialiased leading-snug tracking-normal font-sans">
                     {{heading3}} <span class="text-teal-500">{{subheading3}}</span>
                 </div>
-
-                <div class="relative">
-                    <slot name="toolbox"></slot>
-                </div>
             </div>
-            <div :class="'mt-2 p-3 font-sans text-gray-700 antialiased mb-2'+height">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quidem minus id omnis, nam expedita, ea fuga commodi voluptas iusto, hic autem deleniti dolores explicabo labore enim repellat earum perspiciatis.
+            <div class="relative">
+                <slot name="toolbox2"></slot>
             </div>
         </div>
     </div>
@@ -52,43 +41,40 @@
         props:{
             heading1:{
                 type: String,
-                required: true
+                // required: true
 
 
             },
             heading2:{
                 type: String,
-                required: true
+                // required: true
 
 
             },
             heading3:{
                 type: String,
-                required: true
+                // required: true
 
 
             },
             subheading1:{
                 type: String,
-                required: true
+                // required: true
 
 
             },
             subheading2:{
                 type: String,
-                required: true
+                // required: true
 
 
             },
-            subheading3:{
+            subheading3: {
                 type: String,
-                required: true
+                // required: true
 
 
-            },
-            height: {
-                type: Number,
-            },
+            }
         }
     }
 </script>
