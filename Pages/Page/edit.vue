9acd9b67607d012317157eeb6f19f1d111dcf97a
Rubal Gulati <rubal@noetic.com>
2020-07-03 19:45:21 +0530

    form builder

--- Pages/Page/edit.vue
+++ Pages/Page/edit.vue
@@ -130,9 +130,17 @@
                                             </div>
 
                                             <div v-for="(element, index) in elements">
-                                                <a href="#" v-model="element.type" class="flex justify-start hover:bg-gray-400 inline-block max-w-xs w-48 m-4 px-4 py-2 bg-gray-300 focus:outline-none focus:shadow-outline  text-sm tracking-wider">
+                                                <div v-model="element.type" class="hover-trigger flex justify-start hover:bg-gray-400 inline-block max-w-xs w-48 m-4 px-4 py-2 bg-gray-300 focus:outline-none focus:shadow-outline  text-sm tracking-wider">
                                                     Text on the Button
-                                                </a>
+                                                    <div class="absolute bg-teal-400 border border-grey-100 px-2 hover-target">
+                                                        <div @click="showEditModal(element.type)" class="text-gray-400 hover:text-white inline-block font-bold">
+                                                            <svg class="h-4 w-4 my-2 mx-3 font-bold" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 477.873 477.873"><path fill-rule="evenodd" d="M392.533 238.937c-9.426 0-17.067 7.641-17.067 17.067V426.67c0 9.426-7.641 17.067-17.067 17.067H51.2c-9.426 0-17.067-7.641-17.067-17.067V85.337c0-9.426 7.641-17.067 17.067-17.067H256c9.426 0 17.067-7.641 17.067-17.067S265.426 34.137 256 34.137H51.2C22.923 34.137 0 57.06 0 85.337V426.67c0 28.277 22.923 51.2 51.2 51.2h307.2c28.277 0 51.2-22.923 51.2-51.2V256.003c0-9.425-7.641-17.066-17.067-17.066z"/><path d="M458.742 19.142A65.328 65.328 0 00412.536.004a64.85 64.85 0 00-46.199 19.149L141.534 243.937a17.254 17.254 0 00-4.113 6.673l-34.133 102.4c-2.979 8.943 1.856 18.607 10.799 21.585 1.735.578 3.552.873 5.38.875a17.336 17.336 0 005.393-.87l102.4-34.133c2.515-.84 4.8-2.254 6.673-4.13l224.802-224.802c25.515-25.512 25.518-66.878.007-92.393z"/></svg>
+                                                        </div>
+                                                        <div  class="text-gray-400 hover:text-white inline-block font-bold">
+                                                            <svg class="h-4 w-4 my-2 mx-3 font-bold" fill="currentColor" viewBox="0 0 512.001 512.001" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M512.001 84.853L427.148 0 256.001 171.147 84.853 0 0 84.853 171.148 256 0 427.148l84.853 84.853 171.148-171.147 171.147 171.147 84.853-84.853L340.853 256z"/></svg>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
 
                                         </div>
@@ -691,19 +699,21 @@
                     this.$delete(this.addRow, arrayIndex)
                 }
             },
+            showEditModal(type){
+                if(type === 'button'){
+                    this.Add = true
+                }
+            },
             showModal(type){
                 if(type === 'row')
                 {
-
                     this.addedRow = true
                     this.isOpen2 = false
                     this.noData = false
                 }
-
                 else {
 
-                    this.elements.push({type: ''});
-
+                    this.elements.push({type: type});
                     this.inlineBlock = false
                     this.addedRow = true
                     this.isOpen2 = false
@@ -727,5 +737,13 @@
 </script>
 
 <style scoped>
+
+    .hover-trigger .hover-target {
+        display: none;
+    }
+
+    .hover-trigger:hover .hover-target {
+        display: block;
+    }
 
 </style>
