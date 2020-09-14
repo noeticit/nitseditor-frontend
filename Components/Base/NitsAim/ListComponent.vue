3287ee76990d40050b1c3c76a11aaf30c321d9b8
abuzaid sayyad <abuzaidsayyad86@gmail.com>
2020-09-14 21:24:19 +0530

    some components

--- Components/Base/NitsAim/ListComponent.vue
+++ Components/Base/NitsAim/ListComponent.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <div class="h-screen overflow-y-scroll">
+    <div class="w-full py-1 px-6 flex mb-24">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ Components/Base/NitsAim/NitsFlex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex justify-center">
+    <div class="m-1 rounded-lg shadow-lg bg-white w-10/12">
+      <div class="flex border-b p-2 text-center items-center">
+        <div class="text-left ml-6 py-3 text-xl text-gray-700 font-semibold antialiased leading-snug tracking-normal font-sans">{{name}}</div>
+        <div @click="redirect(path)" class="text-left ml-2 py-3 text-xl text-indigo-500 font-medium antialiased leading-snug tracking-normal cursor-pointer font-sans">{{link}}</div>
+      </div>
+      <div class="m-4">
+
+        <slot></slot>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "NitsFlex",
+  props:{
+    name:String,
+    path:String,
+    link:String,
+  },
+  methods:{
+    redirect(path){
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
