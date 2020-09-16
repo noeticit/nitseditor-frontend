d07f97743b6d216ba91aeb230c014152ff102525
Rubal Gulati <rubal@noetic.com>
2020-09-16 20:23:14 +0530

    components

--- Components/App/Features/NitsFeatureFour.vue
+++ Components/App/Features/NitsFeatureFour.vue
@@ -1,10 +1,37 @@
 <template>
-    
+    <nits-column :gap="4" :cols="1">
+        <div class="w-full">
+            <div class="text-center text-4xl font-bold text-blue-800 lg:px-32">
+                <div class="lg:px-48 tracking-wide font-ubuntu">{{title}}</div>
+            </div>
+            <ul class="mx-10 flex justify-center flex-wrap">
+                <nits-grid :cols="per_row" :gap="4">
+                    <li class="mx-5" v-for="(item,index) in content">
+                        <div class="h-20 w-20 text-blue-800 hover:text-blue-600 hover:bg-blue-200 border-4 border-blue-200 flex mx-auto rounded-full">
+                            <svg class="mx-auto my-auto h-10 w-10" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.001 512.001">
+                                <path fill-rule="evenodd" :d="item.icon"/>
+                                <path v-if="item.path" :d="item.path"></path>
+                            </svg>
+                        </div>
+                        <div class="text-center text-blue-800 hover:text-blue-600 font-ubuntu text-lg py-6 font-semibold">{{item.name}}</div>
+                    </li>
+                </nits-grid>
+            </ul>
+        </div>
+    </nits-column>
 </template>
 
 <script>
     export default {
-        name: "NitsFeatureFour"
+        name: "NitsFeatureFour",
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            content: Array,
+            per_row: Number
+        },
     }
 </script>
 
--- Components/App/Features/NitsFeatureOne.vue
+++ Components/App/Features/NitsFeatureOne.vue
@@ -1,11 +1,11 @@
 <template>
     <nits-column :gap="4" :cols="1" class="md:grid-cols-2">
-        <div class="lg:mx-8 mx-4" v-if="location === 'left'">
+        <div :class="'lg:mx-8 mx-4 md:mt-1 '+ (image_margin_top ? 'mt-'+image_margin_top : '')" v-if="location === 'left'">
             <img class="" :src="src" :alt="alt"/>
         </div>
-        <div class="lg:mx-12 mx-6 lg:mt-24 md:mt-16 mt-24">
-            <h2 class="text-blue-800 font-bold text-5xl font-ubuntu">{{title}}</h2>
-            <div class="content-center text-left leading-tight text-blue-800 text-3xl mt-5 font-ubuntu antialiased tracking-normal font-semibold">{{subtitle}}</div>
+        <div class="lg:mx-12 mx-6 lg:mt-24 md:mt-16 mt-16">
+            <h2 :class="'text-blue-800 font-bold md:mt-8 text-5xl font-ubuntu ' + (this.textalign ? this.textalign : 'text-left')">{{title}}</h2>
+            <div :class="'content-center leading-tight text-blue-800 text-3xl mt-5 font-ubuntu antialiased tracking-normal font-semibold '+ (this.textalign ? this.textalign : 'text-left')">{{subtitle}}</div>
             <div class="text-gray-800 px-1 mt-5 font-medium text-xl font-ubuntu">{{content}}</div>
         </div>
         <div class="lg:mx-8 mx-4" v-if="location === 'right'">
@@ -31,7 +31,9 @@
             },
             alt: {
                 type: String
-            }
+            },
+            textalign: String,
+            image_margin_top: Number
         },
     }
 </script>
