9b058cb5ffb0c00cffed3273137e767db42253ac
abuzaid-sayyad <abuzaidsayyad86@gmail.com>
2020-10-10 18:17:27 +0530

    changes in few components for responsive UI

--- Components/App/Portfolios/NitsPortfolioOne.vue
+++ Components/App/Portfolios/NitsPortfolioOne.vue
@@ -1,25 +1,39 @@
 <template>
-    <nits-column class="w-full lg:px-10 px-4" :gap="4" :col="1">
+    <nits-column class="w-full lg:px-10 px-2" :gap="4" :col="1">
         <div class="lg:mt-16 mt-8">
             <div class="text-blue-800 tracking-wide font-bold md:text-4xl text-2xl text-center font-ubuntu">{{title}}</div>
         </div>
-        <div class="flex flex-wrap mt-8">
-            <div v-for="item in content" class="flex w-1/2 md:w-1/3 lg:w-1/6 lg:m-5 p-2 md:p-5 stream-container">
-                <a class=" w-full">
-                    <div class=" relative">
-                        <div class="poster-container">
-                            <img class="poster w-full" :src="item.image">
-                            <img class="h-26 w-16 absolute bottom-0 lg:ml-16 ml-12 -mb-6" :src="item.icon">
+        <div class="flex flex-wrap mt-8 mx-2 md:px-16">
+          <div class="grid gap-2 md:gap-10 grid-cols-3 sm:grid-cols-3 lg:grid-cols-5 md:mt-5 mt-3">
+            <div v-for="item in content" >
+                <a class="w-full">
+                    <div class="relative">
+                      <div class="poster-container">
+                        <div class="flex justify-center">
+                          <img class="poster h-24 w-24 md:w-64 md:h-56" :src="item.image">
+
                         </div>
+                        <div class="absolute bottom-0 flex justify-center w-full">
+                          <div class="md:w-24 md:h-24 md:p-3 h-12 w-12 p-2 md:-mb-10 -mb-6">
+                            <img class="w-full h-full" :src="item.icon">
+
+                          </div>
+
+                        </div>
+                      </div>
                     </div>
 
-                    <div class="mt-3 text-center text-xl font-semibold antialiased  text-blue-800 tracking-normal font-ubuntu">
-                        <div class="pt-3">
-                            {{item.title}}
-                        </div>
+                    <div class="md:mt-4 mt-3 text-center text-sm md:text-xl font-semibold antialiased  text-blue-800 tracking-normal font-ubuntu">
+                      <div class="md:pt-4 pt-2">
+                        {{item.title}}
+                      </div>
                     </div>
-                </a>
+                  </a>
+
+
+
             </div>
+          </div>
         </div>
     </nits-column>
 </template>
