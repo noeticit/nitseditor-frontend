3ef0e0c8211ff66abc7cb271246b16cbd427167f
Nitish Kumar <nitishkumar@noeticitservices.com>
2020-12-04 10:59:52 +0530

    CKEditor removed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,36 +1,36 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
-const tailwindcss = require('tailwindcss');
+// const tailwindcss = require('tailwindcss');
 
 const NitsComponentsPlugin = require('./Webpack/NitsComponentsPlugin');
-const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
-const CKEStyles = require('@ckeditor/ckeditor5-dev-utils').styles;
-const CKERegex = {
-    svg: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-    css: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/,
-};
+// const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+// const CKEStyles = require('@ckeditor/ckeditor5-dev-utils').styles;
+// const CKERegex = {
+//     svg: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+//     css: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/,
+// };
 
-Mix.listen('configReady', webpackConfig => {
-    const rules = webpackConfig.module.rules;
-    const targetSVG = /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/;
-    const targetFont = /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/;
-    const targetCSS = /\.css$/;
-
-    // exclude CKE regex from mix's default rules
-    // if there's a better way to loop/change this, open to suggestions
-    for (let rule of rules) {
-        if (rule.test.toString() === targetSVG.toString()) {
-            rule.exclude = CKERegex.svg;
-        }
-        else if (rule.test.toString() === targetFont.toString()) {
-            rule.exclude = CKERegex.svg;
-        }
-        else if (rule.test.toString() === targetCSS.toString()) {
-            rule.exclude = CKERegex.css;
-        }
-    }
-});
+// Mix.listen('configReady', webpackConfig => {
+//     const rules = webpackConfig.module.rules;
+//     const targetSVG = /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/;
+//     const targetFont = /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/;
+//     const targetCSS = /\.css$/;
+//
+//     // exclude CKE regex from mix's default rules
+//     // if there's a better way to loop/change this, open to suggestions
+//     for (let rule of rules) {
+//         if (rule.test.toString() === targetSVG.toString()) {
+//             rule.exclude = CKERegex.svg;
+//         }
+//         else if (rule.test.toString() === targetFont.toString()) {
+//             rule.exclude = CKERegex.svg;
+//         }
+//         else if (rule.test.toString() === targetCSS.toString()) {
+//             rule.exclude = CKERegex.css;
+//         }
+//     }
+// });
 
 
 mix.sass('./resources/sass/app.scss', 'public/nits-assets/css')
@@ -39,34 +39,6 @@ mix.sass('./resources/sass/app.scss', 'public/nits-assets/css')
         require("tailwindcss"),
     ])
     .webpackConfig({
-        module: {
-            rules: [
-                {
-                    test: CKERegex.svg,
-                    use: [ 'raw-loader' ]
-                },
-                {
-                    test: CKERegex.css,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                            options: {
-                                singleton: true
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: CKEStyles.getPostCssConfig({
-                                themeImporter: {
-                                    themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
-                                },
-                                minify: true
-                            })
-                        },
-                    ]
-                }
-            ]
-        },
         output: {
             chunkFilename: 'nits-assets/chunks/[name].[contenthash].js'
         },
@@ -80,7 +52,6 @@ mix.sass('./resources/sass/app.scss', 'public/nits-assets/css')
             },
         },
         plugins: [
-            new CKEditorWebpackPlugin({language: 'en', additionalLanguages: 'all'}),
             new NitsComponentsPlugin(),
         ]
     }).babelConfig({ plugins: ['@babel/plugin-syntax-dynamic-import']}).sourceMaps().version();
